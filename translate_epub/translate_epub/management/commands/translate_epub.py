df0cfd7c8d5e264ca83457cb6edb8ee001dd9adb
Danny Boy <orienta.dan@gmail.com>
2024-06-15 17:36:07 +0900

    translate_epub before trying to upgrade to openai v1

---
+++ translate_epub/translate_epub/management/commands/translate_epub.py
@@ -0,0 +1,154 @@
+import argparse
+# from openai import OpenAI
+# from openai import AzureOpenAI
+import openai
+import environ
+from bs4 import BeautifulSoup as bs
+from ebooklib import epub
+from rich import print
+
+from django.core.management.base import BaseCommand
+
+env = environ.Env()
+environ.Env.read_env()
+
+API_BASE = env('API_BASE')
+API_KEY = env('API_KEY')
+API_MODEL = env('API_MODEL')
+
+class Command(BaseCommand):
+    help = 'Translates an .epub'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--lang_to',
+            dest='lang_to',
+            type=str,
+            help='translate the epub to language',
+        )
+        parser.add_argument(
+            '--lang_from',
+            dest='lang_from',
+            type=str,
+            help='translate the epub from language',
+        )
+        parser.add_argument(
+            '--book_name',
+            dest='book_name',
+            type=str,
+            help='your epub book name',
+        )
+
+    def handle(self, *args, **options):
+        if not options['book_name'].endswith('.epub'):
+            raise Exception('This program translates .epub files only.')
+        if not options['lang_to'] or not options['lang_from']:
+            raise Exception('needs --lang_to and --lang_from')
+        e = TEPUB(options['book_name'], options['lang_from'], options['lang_to'])
+        e.translate_book()
+
+class ChatGPT:
+    # def __init__(self):
+
+    def translate(self, text, lang_from, lang_to):
+        print('Original Text:')
+        # print('"' + text + '"')
+        print(text)
+        if not text.strip():
+            return ''
+        openai.api_base=API_BASE
+        openai.api_key = API_KEY
+        # openai.api_base = API_BASE
+        # print(OpenAI.api_base)
+        language_to = lang_to
+        language_from = lang_from
+        try:
+            completion = openai.Completion.create(
+                model=API_MODEL,
+                messages=[
+                    {
+                        "role": "user",
+                        "content": f"Translate the following text from {language_from} into {language_to}.\n {language_from}: {text}\n{language_to}:"
+                    },
+                ],
+            )
+            t_text = (
+                completion["choices"][0]
+                .get("message")
+                .get("content")
+                .encode("utf8")
+                .decode()
+            )
+        except Exception as e:
+            print(str(e))
+
+        print('Translation:')
+        print(t_text)
+        return t_text
+
+
+class TEPUB:
+    def __init__(self, epub_name, lang_from, lang_to):
+        self.epub_name = epub_name
+        self.translate_model = ChatGPT()
+        self.origin_book = epub.read_epub(self.epub_name)
+        self.lang_from = lang_from
+        self.lang_to = lang_to
+
+    def translate_book(self):
+        new_book = epub.EpubBook()
+        new_book.metadata = self.origin_book.metadata
+        new_book.spine = self.origin_book.spine
+        new_book.toc = self.origin_book.toc
+        for i in self.origin_book.get_items():
+            if i.get_type() == 9:
+                soup = bs(i.content, "html.parser")
+                p_list = soup.findAll("p")
+                for p in p_list:
+                    if p.text and not p.text.isdigit():
+                        translation = self.translate_model.translate(p.text, self.lang_from, self.lang_to)
+                        # Split translation into lines and create HTML with <br> for line breaks
+                        translation_lines = translation.split('\n')
+                        new_p_contents = soup.new_tag("span")  # Using span to insert HTML content inside p tag
+                        for line in translation_lines:
+                            if new_p_contents.contents:  # If not the first line, add a <br> before adding next line
+                                new_p_contents.append(soup.new_tag("br"))
+                            new_p_contents.append(soup.new_string(line))
+                        new_p = soup.new_tag("p")
+                        new_p.insert(0, new_p_contents)
+                        p.insert_after(new_p)
+                i.content = soup.prettify().encode()
+            new_book.add_item(i)
+
+        name = self.epub_name.split(".")[0]
+        epub.write_epub(f"{name}_translated.epub", new_book, {})
+
+
+# if __name__ == "__main__":
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument(
+#         "--lang_to",
+#         dest="lang_to",
+#         type=str,
+#         help="translate the epub to language",
+#     )
+#     parser.add_argument(
+#         "--lang_from",
+#         dest="lang_from",
+#         type=str,
+#         help="translate the epub from language",
+#     )
+#     parser.add_argument(
+#         "--book_name",
+#         dest="book_name",
+#         type=str,
+#         help="your epub book name",
+#     )
+#     options = parser.parse_args()
+#     if not options.book_name.endswith(".epub"):
+#         raise Exception("This program translates .epub files only.")
+#     if not options.lang_to or not options.lang_from:
+#         raise Exception("needs --lang_to and --lang_from")
+#     e = TEPUB(options.book_name, options.lang_from, options.lang_to)
+#     e.translate_book()
+
